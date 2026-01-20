55fe1df4b0d9da0bdd14a10802c60e4b3b53f46b
Mateusz Rojek <merkius@wp.pl>
2026-01-21 00:54:54 +0100

    feat: configure global settings for @nestjs/config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,10 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost/nest',
     ),

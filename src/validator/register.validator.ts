54f8a09ec6220310ea476ce7b0ccc720f23c31f0
Mateusz Rojek <merkius@wp.pl>
2026-01-28 23:51:52 +0100

    feat: add RegisterValidator for user registration validation

---
+++ src/validator/register.validator.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { UserErrorCode } from '../exception/error/UserErrorCode';
+
+export interface ValidationError {
+  field: string;
+  code: UserErrorCode;
+}
+
+export interface RegistrationData {
+  username: string;
+  password: string;
+}
+
+@Injectable()
+export class RegisterValidator {
+  validateRegistration(data: RegistrationData): ValidationError[] {
+    const errors: ValidationError[] = [];
+
+    if (!data.username || data.username.toString().trim() === '') {
+      errors.push({
+        field: 'username',
+        code: UserErrorCode.MISSING_USERNAME,
+      });
+    } else if (data.username.length < 3) {
+      errors.push({
+        field: 'username',
+        code: UserErrorCode.USERNAME_TOO_SHORT,
+      });
+    }
+
+    if (!data.password || data.password.toString().trim() === '') {
+      errors.push({
+        field: 'password',
+        code: UserErrorCode.MISSING_PASSWORD,
+      });
+    } else if (data.password.length < 6) {
+      errors.push({
+        field: 'password',
+        code: UserErrorCode.PASSWORD_TOO_SHORT,
+      });
+    }
+
+    return errors;
+  }
+}

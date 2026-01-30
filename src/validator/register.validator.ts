5adc2a49e1598ca05da7992a6b19359dad916161
vMerkius <merkius@wp.pl>
2026-01-31 00:14:33 +0100

    refactor: simplify error handling in registration validation

--- src/validator/register.validator.ts
+++ src/validator/register.validator.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UserErrorCode } from '../exception/error/UserErrorCode';
+import { error } from 'console';
 
 export interface IValidationError {
   field: string;
@@ -13,41 +14,53 @@ export interface IRegistrationData {
 
 @Injectable()
 export class RegisterValidator {
-  errors: IValidationError[] = [];
   validateRegistration(
     data: IRegistrationData,
     isUsernameTaken: boolean | null = null,
   ): IValidationError[] {
+    const errors: IValidationError[] = [];
+
+    this.validateUsername(data.username, errors, isUsernameTaken);
+    this.validatePassword(data.password, errors);
+
+    return errors;
+  }
+
+  validateUsername(
+    username: string,
+    errors: IValidationError[],
+    isUsernameTaken?: boolean | null,
+  ): void {
     if (isUsernameTaken) {
-      this.errors.push({
+      errors.push({
         field: 'username',
         code: UserErrorCode.USER_ALREADY_EXISTS,
       });
     }
-    if (!data.username || data.username.toString().trim() === '') {
-      this.errors.push({
+    if (!username || username.trim() === '') {
+      errors.push({
         field: 'username',
         code: UserErrorCode.MISSING_USERNAME,
       });
-    } else if (data.username.length < 3) {
-      this.errors.push({
+    } else if (username.length < 3) {
+      errors.push({
         field: 'username',
         code: UserErrorCode.USERNAME_TOO_SHORT,
       });
     }
+  }
 
-    if (!data.password || data.password.toString().trim() === '') {
-      this.errors.push({
+  validatePassword(password: string, errors: IValidationError[]): void {
+    if (!password || password.trim() === '') {
+      errors.push({
         field: 'password',
         code: UserErrorCode.MISSING_PASSWORD,
       });
-    } else if (data.password.length < 6) {
-      this.errors.push({
+    } else if (password.length < 6) {
+      errors.push({
         field: 'password',
         code: UserErrorCode.PASSWORD_TOO_SHORT,
       });
     }
-
-    return this.errors;
   }
 }

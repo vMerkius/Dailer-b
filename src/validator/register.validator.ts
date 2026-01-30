c7d437d30bcba15d29c9fb0e5d50c15236e46745
vMerkius <merkius@wp.pl>
2026-01-30 23:51:30 +0100

    refactor: streamline error handling in registration validation

--- src/validator/register.validator.ts
+++ src/validator/register.validator.ts
@@ -13,33 +13,32 @@ export interface IRegistrationData {
 
 @Injectable()
 export class RegisterValidator {
+  errors: IValidationError[] = [];
   validateRegistration(data: IRegistrationData): IValidationError[] {
-    const errors: IValidationError[] = [];
-
     if (!data.username || data.username.toString().trim() === '') {
-      errors.push({
+      this.errors.push({
         field: 'username',
         code: UserErrorCode.MISSING_USERNAME,
       });
     } else if (data.username.length < 3) {
-      errors.push({
+      this.errors.push({
         field: 'username',
         code: UserErrorCode.USERNAME_TOO_SHORT,
       });
     }
 
     if (!data.password || data.password.toString().trim() === '') {
-      errors.push({
+      this.errors.push({
         field: 'password',
         code: UserErrorCode.MISSING_PASSWORD,
       });
     } else if (data.password.length < 6) {
-      errors.push({
+      this.errors.push({
         field: 'password',
         code: UserErrorCode.PASSWORD_TOO_SHORT,
       });
     }
 
-    return errors;
+    return this.errors;
   }
 }

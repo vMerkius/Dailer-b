1d8b6b19caad1710535fa9fcc2756169819297fb
vMerkius <merkius@wp.pl>
2026-01-31 21:56:51 +0100

    feat: enhance user registration to set access token in cookies

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import type { Response } from 'express';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Res,
+  HttpCode,
+} from '@nestjs/common';
 import { UserService } from '../../service/user/user.service';
 import { CreateUserDto } from '../../integration/user/dto/create-user.dto';
 
@@ -6,9 +15,25 @@ import { CreateUserDto } from '../../integration/user/dto/create-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @HttpCode(201)
   @Post('register')
-  register(@Body() createUserDto: CreateUserDto) {
-    return this.userService.register(createUserDto);
+  async register(
+    @Body() createUserDto: CreateUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.userService.register(createUserDto);
+
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    return {
+      message: 'Registration successful',
+      statusCode: 201,
+    };
   }
 
   @Post('login')

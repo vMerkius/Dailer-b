f91a6575683b9a54c5100d389bd3ea6f6538cfe0
vMerkius <merkius@wp.pl>
2026-01-31 21:56:55 +0100

    feat: add _id property to User schema for ObjectId support

--- src/model/user/user.schema.ts
+++ src/model/user/user.schema.ts
@@ -1,10 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  _id: Types.ObjectId;
+
   @Prop({ required: true })
   username: string;
 

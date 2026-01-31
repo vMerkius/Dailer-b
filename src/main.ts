b0f2e6ddc6174005d3d4bfd0498e22a8acf23ea1
vMerkius <merkius@wp.pl>
2026-01-31 21:56:47 +0100

    feat: add cookie-parser dependency and integrate it in main application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './module/app.module';
 import { Logger } from '@nestjs/common';
+import { AppModule } from './module/app.module';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
 
   try {
     const app = await NestFactory.create(AppModule);
+
+    app.use(cookieParser());
 
     const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/nest';
     logger.log(`MongoDB URI: ${mongoUri}`);

407ceabf481c9f68ca234786549320aa93e6d7b7
Mateusz Rojek <merkius@wp.pl>
2026-02-02 22:21:36 +0100

    refactor: update UserRepository to use RegisterUserDto and rename findByCredentials to findByUsername

--- src/repository/user/user.repository.ts
+++ src/repository/user/user.repository.ts
@@ -2,18 +2,18 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/model';
-import { CreateUserDto } from 'src/integration';
+import { RegisterUserDto } from 'src/integration';
 
 @Injectable()
 export class UserRepository {
   constructor(@InjectModel('User') private userModel: Model<User>) {}
 
-  async create(data: CreateUserDto): Promise<User> {
+  async create(data: RegisterUserDto): Promise<User> {
     const newUser = new this.userModel(data);
     return await newUser.save();
   }
 
-  async findByCredentials(username: string): Promise<User | null> {
+  async findByUsername(username: string): Promise<User | null> {
     return await this.userModel.findOne({ username });
   }
 

ceaaacfba02425da87a99d42d73241586b4bdb65
vMerkius <merkius@wp.pl>
2026-02-02 14:37:34 +0100

    feat: add GeneralErrorCode enum and corresponding error messages for unexpected errors

---
+++ src/exception/error/GeneralErrorCode.ts
@@ -0,0 +1,15 @@
+import { HttpStatus } from '@nestjs/common';
+
+export enum GeneralErrorCode {
+  GENERAL_ERROR = 'ERR_GENERAL',
+}
+
+export const GENERAL_ERROR_MESSAGES: Record<
+  GeneralErrorCode,
+  { message: string; statusCode: HttpStatus }
+> = {
+  [GeneralErrorCode.GENERAL_ERROR]: {
+    message: 'An unexpected error occurred',
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+  },
+};

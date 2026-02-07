d1bbf98d48479957c0658f65bad2604a807c5333
Mateusz Rojek <merkius@wp.pl>
2026-02-08 00:19:46 +0100

    feat: add AssistantService provider to AssistantModule

--- src/module/assistant/asssitant.module.ts
+++ src/module/assistant/asssitant.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
 import { AssistantController } from 'src/controller';
+import { AssistantService } from 'src/service';
 
 @Module({
   imports: [UserModule],
   controllers: [AssistantController],
-  providers: [],
+  providers: [AssistantService],
 })
 export class AssistantModule {}

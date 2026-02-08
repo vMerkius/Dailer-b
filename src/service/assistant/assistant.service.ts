91ca2a9980bd3b55b07a906de077bc51ceb30beb
Mateusz Rojek <merkius@wp.pl>
2026-02-08 13:09:29 +0100

    feat: enhance error handling in AssistantService and refactor getResponse method

--- src/service/assistant/assistant.service.ts
+++ src/service/assistant/assistant.service.ts
@@ -1,22 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { OpenRouter } from '@openrouter/sdk';
+import { MessageResourceConfig } from 'src/config/MessageResourceConfig';
+import { AssistantErrorCode } from 'src/exception/error';
 import { IOpenRouterResponse } from 'src/types';
 
 @Injectable()
 export class AssistantService {
+  constructor(private messageResourceConfig: MessageResourceConfig) {}
+
   private readonly openRouter = new OpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY!,
-    debugLogger: console,
+    // debugLogger: console,
   });
 
   async getResponse(prompt: string): Promise<unknown> {
-    const completion: IOpenRouterResponse = await this.openRouter.chat.send({
-      chatGenerationParams: {
-        model: 'openrouter/free',
-        messages: [{ role: 'user', content: prompt }],
-        stream: false,
-      },
+    try {
+      const completion: IOpenRouterResponse | undefined =
+        await this.openRouter.chat.send({
+          chatGenerationParams: {
+            model: 'openrouter/free',
+            messages: [{ role: 'user', content: prompt }],
+            stream: false,
+          },
+        });
+      if (!completion) {
+        this.throwResponseError();
+      }
+
+      return completion;
+    } catch (error) {
+      console.error('Error in AssistantService.getResponse:', error);
+      this.throwResponseError();
+    }
+  }
+
+  private throwResponseError(): never {
+    throw new InternalServerErrorException({
+      code: AssistantErrorCode.RESPONSE_ERROR,
+      message: this.messageResourceConfig.getMessage(
+        AssistantErrorCode.RESPONSE_ERROR,
+      ),
+      statusCode: this.messageResourceConfig.getStatusCode(
+        AssistantErrorCode.RESPONSE_ERROR,
+      ),
     });
-    return completion;
   }
 }

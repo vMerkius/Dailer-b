d7dad4ac257a081d31181ab0ca0eb829972d3c24
Mateusz Rojek <merkius@wp.pl>
2026-02-08 13:09:33 +0100

    feat: add MessageResourceConfig to AssistantModule providers

--- src/module/assistant/asssitant.module.ts
+++ src/module/assistant/asssitant.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
 import { AssistantController } from 'src/controller';
 import { AssistantService } from 'src/service';
+import { MessageResourceConfig } from 'src/config/MessageResourceConfig';
 
 @Module({
   imports: [UserModule],
   controllers: [AssistantController],
-  providers: [AssistantService],
+  providers: [AssistantService, MessageResourceConfig],
 })
 export class AssistantModule {}
